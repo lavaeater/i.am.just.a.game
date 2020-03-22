b80b6d05bd2d4979978150e7141ecb973efbd55d
Tommie Nygren <tommie.nygren@gmail.com>
2020-03-22 17:13:45 +0100

    First commit

---
+++ core/src/main/java/the/game/package/MainGameClass.java
@@ -0,0 +1,7 @@
+package the.game.package;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class MainGameClass extends ApplicationAdapter {
+}

ee688ca5b3c7e74bbc11bf0aa87d6e90dd90b496
Tommie Nygren <tommie.nygren@gmail.com>
2020-03-24 18:18:05 +0100

    Added adjustable time progression

--- core/src/main/kotlin/systems/TimeSystem.kt
+++ core/src/main/kotlin/systems/TimeSystem.kt
@@ -9,14 +9,16 @@ import java.time.format.FormatStyle
 /*
 Keeps track of Time of day and date and stuff because fun fun!
  */
-class TimeSystem(startTime: Int = 6) : IntervalSystem(5f) {
+
+
+class TimeSystem(startTime: Int = 6, interval: Float = 5f, private val minutesPerTick: Long = 60) : IntervalSystem(interval) {
 
     init {
         currentDateTime = LocalDateTime.of(2020, 1,1, startTime,0)
     }
 
     override fun updateInterval() {
-        currentDateTime =  currentDateTime.plusHours(1)
+        currentDateTime =  currentDateTime.plusMinutes(minutesPerTick)
         info { currentDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)) }
     }
     companion object {
